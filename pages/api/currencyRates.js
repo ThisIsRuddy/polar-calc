aa207018049515222d7d5af6deacb6726ece9909
Daniel Watts <thisisruddy@gmail.com>
2022-04-30 00:00:40 +0100

    Scaffolded cached currency rates - ready for front-end wiring

---
+++ pages/api/currencyRates.js
@@ -0,0 +1,86 @@
+import fs from 'fs';
+import util from 'util';
+import axios from 'axios';
+
+import minutesBetween from '../../lib/minutesBetween';
+import addMinutes from '../../lib/addMinutes';
+
+const readFile = util.promisify(fs.readFile);
+const statFile = util.promisify(fs.stat);
+const writeFile = util.promisify(fs.writeFile);
+
+const fetchCurrencyRates = async (baseCurrency, symbols) => await axios.get('https://api.apilayer.com/exchangerates_data/latest', {
+  timeout: 5000,
+  redirect: 'follow',
+  headers: {
+    apikey: process.env.APILAYER_API_KEY
+  },
+  params: {
+    base: baseCurrency,
+    symbols: symbols.join(',')
+  }
+});
+
+const readCachedRates = async () => {
+  const data = await readFile(__dirname + '/currencyRates.json');
+  return JSON.parse(data);
+}
+
+const statCachedRates = async () => await statFile(__dirname + '/currencyRates.json');
+
+const cacheRates = async (rates) => await writeFile(__dirname + '/currencyRates.json', JSON.stringify(rates, null, 2));
+
+const updateRates = async () => {
+  const {data} = await fetchCurrencyRates('USD', [
+    "AUD", "BTC", "GBP", "CAD", "EUR", "HKD", "NZD", "CHF", "AED"
+  ])
+  console.info("[currencyRates:updateRates] Fetch rates: OK");
+
+  const now = new Date();
+  const rates = {
+    ...data['rates'],
+    tlu: now,
+    ttu: addMinutes(now, 180),
+    mtu: "180 min"
+  }
+  await cacheRates(rates);
+  console.info("[currencyRates:updateRates] Write rates: OK");
+
+  return rates;
+}
+
+const getRates = async () => {
+  let rates;
+
+  try {
+    rates = await readCachedRates();
+    console.info("[currencyRates:getRates] Read cached rates: OK");
+  } catch (e) {
+    console.error("[currencyRates:getRates] Read cached rates: FAIL");
+    return await updateRates();
+  }
+
+  const stats = await statCachedRates();
+  console.info("[currencyRates:getRates] Stat cached rates: OK");
+  const now = new Date();
+  const diff = minutesBetween(stats.mtime, now);
+
+  if (diff > 180)
+    rates = await updateRates();
+
+  return {
+    ...rates,
+    mtu: 180 - diff + " min"
+  };
+}
+
+export default async function handler(req, res) {
+  try {
+    const rates = await getRates();
+    console.info("[currencyRates:handler] Get rates: OK");
+    res.status(200).json(rates);
+  } catch (err) {
+    console.error("[currencyRates:handler] Get rates: FAIL");
+    res.status(500).json({error: "Failed to retrieve currency rates: " + err.message});
+  }
+}
